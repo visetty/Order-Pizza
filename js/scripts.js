b90a31d80defb7b71562f804143bc91419fe764d
visetty <visalalakkimsetty@gmail.com>
2018-07-14 21:32:19 -0700

    adds js for linking delivery type and ordersummary and confirmation of order in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 //backend-logic
 
-function pizzaOrder(size, toppings, delivery, price) {
+function pizzaOrder(size, toppings, delivery) {
   this.size = size;
   this.toppings = toppings;
   this.delivery = delivery;
-  this.price = 0;
 }
 
-pizzaOrder.prototype.price = function() {
+pizzaOrder.prototype.total = function() {
+  this.price=0;
   if (this.size === "small") {
     this.price += 7;
   } else if (this.size === "medium") {
@@ -17,11 +17,15 @@ pizzaOrder.prototype.price = function() {
   } else if (this.size === "X-large") {
     this.price +=13;
   }
+
+  if (this.delivery === "Delivery") {
+  this.price += 2;
+  }
+
+  this.price += this.toppings.length * 1;
+  return this.price;
 }
 
-if (this.delivery === "delivery") {
-  this.price += 2;
-}
 
 
 //user-interface-logic
@@ -37,7 +41,64 @@ $(document).ready(function() {
     });
 
     var deliveryChoice = $("input:radio[name=delivery]:checked").val();
+    var order = new pizzaOrder(sizeSelected, toppingSelected, deliveryChoice);
 
-    //alert(deliveryChoice);
+    order.total();
+
+
+
+    if (deliveryChoice === "Delivery") {
+      $("#customeraddress").show();
+    } else {
+      $("#customeraddress").hide();
+      $("form#orderselection").hide();
+      $("#ordersummary").show();
+      $(".sizeoforder").html( sizeSelected);
+      $(".yourtoppings").html( toppingSelected);
+      $(".totalcost").html(order.total());
+      $(".yourdeliverytype").html(deliveryChoice);
+      $("#ordersummary").show();
+    }
+
+    $("form#customeraddress").submit(function(event) {
+      event.preventDefault();
+      var name=$("input#customername").val();
+      var street=$("input#street").val();
+      var city=$("input#city").val();
+      var state=$("input#state").val();
+      var zipcode=$("input#zipcode").val();
+      $(".name").text(name);
+      $(".street").text(street);
+      $(".city").text(city);
+      $(".state").text(state);
+      $(".zipcode").text(zipcode);
+      //$("#customeraddress").hide();
+      $("#ordersummary").show();
+      $(".sizeoforder").html( sizeSelected);
+      $(".yourtoppings").html( toppingSelected);
+      $(".totalcost").html(order.total());
+      $(".yourdeliverytype").html(deliveryChoice);
+      $("#customeraddress").hide();
+      $("form#orderselection").hide();
+    });
+
+    $("#ordersummary").click(function(event) {
+      event.preventDefault();
+      $("#ordersummary").hide();
+
+      if (deliveryChoice === "Delivery") {
+        $("#customer-delivery").show();
+      } else {
+        $("#customer-pickup").show();
+      }
+    });
+
+
+
+
+
+
+
+
   });
 });
